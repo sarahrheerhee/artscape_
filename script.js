5ce187bf16301157d901cf955ad56683a9d449ef
sarahrheerhee <36961253+sarahrheerhee@users.noreply.github.com>
2020-07-06 15:50:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+jQuery(document).ready(function ($) {
+$(window).load(function () {
+        setTimeout(function(){
+            $('.loader-wrapper').fadeOut(1100, function () {
+            });
+        },3000); // set the time here
+});
+   $(function(){
+   setTimeout(function(){
+     $('.scroll-container').fadeIn(1500);
+   },3000);
+
+
+
+
+
+
+    });  
+
+   
+     });  
+
